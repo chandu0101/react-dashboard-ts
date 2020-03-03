62020ce9b30562b543d72206e5db58221763c631
chandu0101 <chandu.csu2010@gmail.com>
2020-03-03 14:55:15 +0530

    initial commit

---
+++ src/components/RouteWithLayout.tsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+
+type RouteWithLayoutProps = { component: any; layout: any; path?: string };
+
+export const RouteWithLayout: React.FC<RouteWithLayoutProps> = ({
+  layout: Layout,
+  component,
+  ...rest
+}) => {
+  return (
+    <Route
+      render={matchProps => (
+        <Layout>
+          <Component {...matchProps} />
+        </Layout>
+      )}
+      {...rest}
+    />
+  );
+};
